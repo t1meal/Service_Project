1be32ed75458e1c9de47828254cff76b88a4a734
Алексей <t1meal@yandex.ru>
2023-08-29 06:47:41 +0300

    init commit

---
+++ src/main/java/ru/t1meal/service_project/ServiceProjectApplication.java
@@ -0,0 +1,12 @@
+package ru.t1meal.service_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProjectApplication.class, args);
+    }
+}
